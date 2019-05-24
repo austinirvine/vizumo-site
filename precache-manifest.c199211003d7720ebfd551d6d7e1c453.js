cac9040fe02537983e79d77a6c2526965adabe2a
austinirvine <aa1irvine@gmail.com>
2019-05-24 16:21:10 -0500

    Updates

---
+++ precache-manifest.c199211003d7720ebfd551d6d7e1c453.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0f4c8540cdb7d7c4034",
+    "url": "/static/css/main.28713950.chunk.css"
+  },
+  {
+    "revision": "e0f4c8540cdb7d7c4034",
+    "url": "/static/js/main.8e2276fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3acd941e29ab82f3c0d8",
+    "url": "/static/js/2.ca99d448.chunk.js"
+  },
+  {
+    "revision": "a7a66710318c196806dcda548fabde3f",
+    "url": "/static/media/Vizumo.a7a66710.png"
+  },
+  {
+    "revision": "84e5ebbec249ba1607db24f2837a2cbe",
+    "url": "/static/media/modern_home.84e5ebbe.jpg"
+  },
+  {
+    "revision": "70deb7c217f86c653d2b1db1dc4fe665",
+    "url": "/static/media/modern_interior.70deb7c2.jpg"
+  },
+  {
+    "revision": "a9586d41f243b45cdf6d63f0772ae105",
+    "url": "/static/media/small_house.a9586d41.jpg"
+  },
+  {
+    "revision": "dc44c019955e6a0700521a354a4a3654",
+    "url": "/static/media/nice_interior.dc44c019.jpg"
+  },
+  {
+    "revision": "7a8d328a52438d25c7388087ca361336",
+    "url": "/static/media/walkway.7a8d328a.jpg"
+  },
+  {
+    "revision": "404074f2172ce51df29ff074a5ffc327",
+    "url": "/static/media/happy_realtor.404074f2.png"
+  },
+  {
+    "revision": "3d27a2fd280714f6d4b1f0e933ba0dad",
+    "url": "/static/media/sim.3d27a2fd.png"
+  },
+  {
+    "revision": "bfa4fbbf190fa4345edeeb58a3108e6c",
+    "url": "/static/media/noosh.bfa4fbbf.png"
+  },
+  {
+    "revision": "fe04320977ac7f3c31bc0761a30adb2d",
+    "url": "/static/media/aus.fe043209.png"
+  },
+  {
+    "revision": "86b6aebac67a9eb3ce6bd0f1ff89412f",
+    "url": "/static/media/tri.86b6aeba.png"
+  },
+  {
+    "revision": "64ae86723843cbe9a99ca70c39d3b77c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vizumo-site/precache-manifest.89c925fc4519a21b438cbe916cbdeed5.js"
+  "/precache-manifest.c199211003d7720ebfd551d6d7e1c453.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vizumo-site/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
