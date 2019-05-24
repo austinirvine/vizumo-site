7b82e74be420d312c06999642c7a6e3a9fecaa0b
austinirvine <aa1irvine@gmail.com>
2019-05-24 14:50:52 -0500

    Adding vizumo

--- src/App.js
+++ src/App.js
@@ -325,7 +325,7 @@ const topics = [
   class App extends Component {
     render() {
       return (
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
             <div className="app">
                 <header>
                   {/* Global site tag (gtag.js) - Google Analytics */}
